8f263a2ac91a378bdb836379ddb416821a3a2e6f
Arjun Malhotra <arjun.malhotra@armada.ai>
2025-05-06 10:18:12 -0700

    Batch query rabbit hole

--- main.go
+++ main.go
@@ -13,18 +13,26 @@ type FlightMedia struct {
 	DownloadURL string `json:"download_url"`
 	FlightID    string `json:"flight_id"`
 	//MediaUUID   string `json:"media_uuid"`
+	CapturedTime int64 `json:"captured_time"`
 }
 
-type Flight struct {
-	FlightID   string  `json:"flight_id"`
-	TakeoffLat float64 `json:"takeoff_lat"`
-	TakeoffLon float64 `json:"takeoff_long"`
+// type Flight struct {
+// 	FlightID   string  `json:"flight_id"`
+// 	TakeoffLat float64 `json:"takeoff_lat"`
+// 	TakeoffLon float64 `json:"takeoff_long"`
+// }
+
+type FlightTelemetry struct {
+	FlightID  string  `json:"flight_id"`
+	GPSLat    float64 `json:"gps_latitude"`
+	GPSLon    float64 `json:"gps_longitude"`
+	Timestamp int64   `json:"timestamp"`
 }
 
 type JoinedData struct {
 	FlightMedia FlightMedia `json:"FlightMedia"`
-	Flights     Flight      `json:"Flights"`
-	Location    struct {
+	//Flights     Flight      `json:"Flights"`
+	Location struct {
 		Lat float64 `json:"lat"`
 		Lon float64 `json:"lon"`
 	} `json:"location"`
@@ -43,28 +51,42 @@ func main() {
 
 	fmt.Println("Connected to scyllaDB!!")
 	var listFlightMedia = make(map[string][]FlightMedia)
+	batch := gocql.Batch{}
 
-	iter := session.Query(`SELECT flight_id,  download_url FROM flight_media`).Iter()
+	iter := session.Query(`SELECT flight_id, download_url, tounixtimestamp(captured_time) FROM flight_media`).Iter()
 	var fm FlightMedia
 	var mediaCount int
-	for iter.Scan(&fm.FlightID, &fm.DownloadURL) {
+	for iter.Scan(&fm.FlightID, &fm.DownloadURL, &fm.CapturedTime) {
 		listFlightMedia[fm.FlightID] = append(listFlightMedia[fm.FlightID], fm)
+		batch.Entries = append(batch.Entries, gocql.BatchEntry{
+			Stmt:       fmt.Sprintf("SELECT flight_id, gps_latitude, gps_longitude, tounixtimestamp(timestamp) FROM flight_telemetry where flight_id='%s' and  timestamp <= %d order by timestamp desc limit 1;", fm.FlightID, fm.CapturedTime),
+			Idempotent: true,
+		})
 		mediaCount++
 	}
+
 	if err := iter.Close(); err != nil {
 		log.Fatalf("Error fetching flightMedia: %v", err)
 	}
 
 	fmt.Println("mediaCount: ", mediaCount)
 
+	err = session.ExecuteBatch(&batch)
+	if err != nil {
+		log.Fatal("error executing the batch query", err)
+	}
+
 	var joinedData []JoinedData
-	iter = session.Query(`SELECT flight_id, takeoff_lat, takeoff_long FROM flights`).Iter()
-	var f Flight
-	for iter.Scan(&f.FlightID, &f.TakeoffLat, &f.TakeoffLon) {
+
+	// iter = session.Query(`SELECT flight_id, gps_latitude, gps_longitude, tounixtimestamp(timestamp) FROM flight_telemetry where flight_id='4389b50b-866f-46e3-bfc1-62a75a80b784' and  timestamp <= 1712082346764 order by timestamp desc limit 1;`).Iter()
+
+	// iter = session.Query(`SELECT flight_id, gps_latitude, gps_longitude, tounixtimestamp(timestamp) FROM flight_telemetry`).Iter()
+	var f FlightTelemetry
+	for iter.Scan(&f.FlightID, &f.GPSLat, &f.GPSLon, &f.Timestamp) {
 		if medias, exists := listFlightMedia[f.FlightID]; exists {
 
 			for _, v := range medias {
-				joinedData = append(joinedData, JoinedData{FlightMedia: v, Flights: f, Location: struct {
+				joinedData = append(joinedData, JoinedData{FlightMedia: v, Location: struct {
 					Lat float64 `json:"lat"`
 					Lon float64 `json:"lon"`
 				}{
